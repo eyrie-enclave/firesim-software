7b407405c25717a5cca148cb5a5042792727f54b
Dayeol Lee <dayeol@berkeley.edu>
2019-04-20 07:38:07 +0000

    Update submodules

--- workloads/br-base/overlay/root/machine-boot-script.sh
+++ workloads/br-base/overlay/root/machine-boot-script.sh
@@ -2,6 +2,6 @@ cd /root
 insmod keystone-driver.ko
 #/bin/login -f root
 cd /root
-./test-runner.riscv attestation.eapp_riscv eyrie-rt
-#for i in {1..10}; do ./test-runner.riscv attestation.eapp_riscv eyrie-rt; done
+#./test-runner.riscv attestation.eapp_riscv eyrie-rt
+for i in {1..10}; do ./test-runner.riscv attestation.eapp_riscv eyrie-rt; done
 poweroff
