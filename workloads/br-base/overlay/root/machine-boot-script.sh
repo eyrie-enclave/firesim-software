3a08949c1ecf8672cd9b2d1af40e185e67b65d70
Edward Im <edwardim@berkeley.edu>
2019-11-12 22:34:14 +0000

    update boot script to use tests.ke

--- workloads/br-base/overlay/root/machine-boot-script.sh
+++ workloads/br-base/overlay/root/machine-boot-script.sh
@@ -1,7 +1,8 @@
 cd /root
 insmod keystone-driver.ko
+./tests.ke
 #/bin/login -f root
-cd /root
+# cd /root
 #./test-runner.riscv attestation.eapp_riscv eyrie-rt
-for i in {1..10}; do ./test-runner.riscv attestation.eapp_riscv eyrie-rt; done
+# for i in {1..10}; do ./test-runner.riscv attestation.eapp_riscv eyrie-rt; done
 poweroff
